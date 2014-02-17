5f5a84a19742527a194c2573d0a14d7815a3e04c
Atte Isopuro <atte.isopuro@aalto.fi>
2014-02-17 12:08:51 +0200

    JSONify root respinse

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask.json import jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello World"
+    response = {'success': 'nothing here yet'}
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run()
