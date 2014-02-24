157262f33cd7f97989f7194bbc2a5e07f58aedc5
Atte Isopuro <atte.isopuro@aalto.fi>
2014-02-24 13:04:39 +0200

    Added parsing of GET-parameters: limit, field and notfield.

---
+++ test_backend.py
@@ -0,0 +1,59 @@
+from backend import Backend
+from types import DictType
+from werkzeug.datastructures import ImmutableMultiDict
+import logging as log
+
+
+class TestBackend():
+    def setUp(self):
+        log.basicConfig(level=log.DEBUG)
+        self.backend = Backend()
+
+    def tearDown(self):
+        self.backend = None
+
+    def test_init(self):
+        pass
+
+    def test_get_root(self):
+        try:
+            self.backend.get_root()
+        except:
+            assert False
+
+    def test_connection(self):
+        log.info('test_connection')
+        try:
+            obj = self.backend.db.products.find_one()
+            log.info(obj)
+            assert type(obj) is DictType
+        except:
+            assert False
+
+    def test_parse_args(self):
+        args = ImmutableMultiDict()
+        # No args should return empty dictionary
+        assert self.backend.parse_args(args) == {}
+        # Incorrect args should return empty dict
+        args = ImmutableMultiDict([('dfdd', 'frefrf'), (342, 4334)])
+        assert self.backend.parse_args(args) == {}
+
+        # Correct arg should return dict with arg as key
+        args = ImmutableMultiDict([
+            ('limit', 20),
+            ('limit', 15)
+        ])
+        val = self.backend.parse_args(args)
+        assert 'limit' in val
+        # value should be the first passed parameter
+        assert val['limit'] == 20
+
+        # giving a field argument should return dict with fields as key
+        args = args = ImmutableMultiDict([
+            ('field', 'price'),
+            ('notfield', 'description')
+        ])
+        val = self.backend.parse_args(args)
+        assert 'fields' in val
+        # Should be dict
+        assert type(val['fields']) is dict
